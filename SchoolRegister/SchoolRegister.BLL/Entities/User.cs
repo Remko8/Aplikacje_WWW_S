f88f96e9642089c72f536ec0df5b7bed7a5cd6b9
Kropa <mvaugusta@gmail.com>
2020-03-03 11:39:23 +0100

    LAB1

---
+++ SchoolRegister/SchoolRegister.BLL/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+namespace SchoolRegister.BLL.Entities
+{
+    public class User : IdentityUser<int>
+    {
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public DateTime RegistrationDate{get;set;}
+
+
+
+    }
+}
