e5fa06f33229616046f8d109961299c882d251f4
Sylwester K <sylkrusz@wp.p[l>
2020-03-03 11:39:36 +0100

    Lab1

---
+++ SchoolRegister.BLL/Entities/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.BLL.Entities
+{
+    public class Student:User
+    { 
+        public double AverageGrade { get; set; }
+        public IDictionary<string,double>AverageGradePerSubject { get; }
+        public IList<Grade>Grades { get; set; }
+        public Group Group { get; set; }
+
+
+    }
+}
---
+++ SchoolRegister.BLL/Entities/Teacher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.BLL.Entities
+{
+    public class Teacher:User
+    {
+        public IList<Subject> Subject { get; set; }
+        public string Title { get; set; }
+    }
+}
