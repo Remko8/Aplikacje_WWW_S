d09f0b6e58da093fea5eda582e9a665cff9a0de6
Pawel b <pawel@xyz.pl>
2020-03-03 11:38:25 +0100

    LAB1

---
+++ SchoolRegister.BLL/Entities/Grade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.BLL.Entities
+{
+    public class Grade
+    {
+        public DateTime DateOflssue { get; set; }
+        public GradeScale GradeValues { get; set; }
+        public Subject Subject { get; set; }
+    }
+}
---
+++ SchoolRegister.BLL/Entities/student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.BLL.Entities
+{
+    public class Student:User
+    {
+        public double AvergaeGrade { get; }
+        public IDictionary<string,double> AverageGradePerSubjcet { get; }
+        public IList<Grade> Grades { get; set; }
+        public Group Group { get; set; }
+    }
+}
