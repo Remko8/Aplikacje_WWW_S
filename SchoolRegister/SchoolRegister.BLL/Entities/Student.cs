0f9a937faf4f787606c86b67fcbf69f788c79b00
Przemyslaw <przemy@o2.pl>
2020-03-03 11:40:18 +0100

    lab1

---
+++ SchoolRegister/SchoolRegister.BLL/Entities/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.BLL.Entities
+{
+    public class Student : User
+    {
+        public double AverageGrade { get; }
+        public IDictionary<string, double> AverageGradePerSubject { get; }
+        public IList<Grade> Grades { get; set; }
+        public Group Group { get; set; }
+
+
+
+    }
+}
