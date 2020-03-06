c10ddb83522506c9b4e6df5e0263eeed41162df7
Hajcik <sdsds@sds.com.pl>
2020-03-06 14:57:19 +0100

    Lab1 Update

--- SchoolRegister/SchoolRegister.BLL/Entities/Parent.cs
+++ SchoolRegister/SchoolRegister.BLL/Entities/Parent.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +6,6 @@ namespace SchoolRegister.BLL.Entities
 {
     public class Parent
     {
+        public IList<Student> Students { get; set; }
     }
 }
