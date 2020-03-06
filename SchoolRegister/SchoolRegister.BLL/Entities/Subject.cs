38690d1ec410e3d50fa3015a74d969933f9b9f3b
Piotr Bednarek <fakemail@party.com>
2020-03-06 11:46:30 +0100

    Lab1

---
+++ SchoolRegister/SchoolRegister.BLL/Entities/Subject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.BLL.Entities
+{
+    public class Subject
+    {
+        public Teacher Teacher { get; set; }
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public IList<Group> Groups { get; set; }
+        public string Description { get; set; }
+    }
+}
