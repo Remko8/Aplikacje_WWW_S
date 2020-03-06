ad6abb8b66e78b08c374f3d65f4f661653e306da
Marcin_M <ss@ss.com>
2020-03-06 11:46:29 +0100

    Lab1

---
+++ SchoolRegister/SchoolRegister.BLL/Entities/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace SchoolRegister.BLL.Entities
+{
+    public class Student : User
+    {
+        //public Group Group { get; set; }
+
+        //public IList<Grade> Grades { get; set; }
+
+        //[NotMapped]
+        //public double AverageGrade => Math.Round(Grades.Average(g => (int)g.GradeValue),1);
+        //[NotMapped]
+        //public IDictionary<string, double> AverageGradeSubject => Grades
+        //    //.GroupBy(g => g.Subject.Name)
+        //    //.Select(g => new { SubjectName = g.Key, AvgGrade = Math.Round(g.Average)})
+
+
+    }
+}
