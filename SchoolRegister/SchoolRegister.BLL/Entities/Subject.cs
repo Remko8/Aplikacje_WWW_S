0df53a5e14ba06ad62c2f9fe7b06471b57ee6845
Wiktor.P <lubiemaila@cb.com>
2020-03-06 11:46:24 +0100

    lab1

---
+++ SchoolRegister/SchoolRegister.BLL/Entities/Subject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.BLL.Entities
+{
+    public class Subject
+    {
+        public string Name { get; set; }
+    }
+}
