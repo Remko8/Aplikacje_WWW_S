e11de874f118585440474d460f628df187a36cad
Kropa <mvaugusta@gmail.com>
2020-03-10 11:59:21 +0100

    Lab02

---
+++ SchoolRegister/SchoolRegister.DAL/EF/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SchoolRegister.BLL.Entities;
+
+namespace SchoolRegister.DAL.EF
+{
+    public class ApplicationDbContext : IdentityDbContext<User, Role, int>
+    {
+        private readonly ConnectionStringDto _connectionStringDto;
+        //Table properties e.g
+        public virtual DbSet<Grade> Grade { get; set; }
+        public virtual DbSet<Subject> Subject { get; set; }
+        public virtual DbSet<Group> Group { get; set; }
+        public ApplicationDbContext(ConnectionStringDto connectionStringDto)
+        {
+            _connectionStringDto = connectionStringDto;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(_connectionStringDto.ConnectionString);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Fluent API commands
+            modelBuilder.Entity<User>()
+            .ToTable("AspNetUsers")
+            .HasDiscriminator<int>("UserType")
+            .HasValue<Student>(1)
+            .HasValue<Parent>(2)
+            .HasValue<Teacher>(3);
+        }
+    }
+}
+
