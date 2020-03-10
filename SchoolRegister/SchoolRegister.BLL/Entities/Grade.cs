45cb58b984d56611148ada80dc5eea3c00eda8d7
Mateusz Mrozowski <mateje@fejk.pl>
2020-03-10 12:03:50 +0100

    10.03

--- SchoolRegister/SchoolRegister.BLL/Entities/Grade.cs
+++ SchoolRegister/SchoolRegister.BLL/Entities/Grade.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace SchoolRegister.BLL.Entities
 {
     public class Grade 
     {
+        [Key]
         public DateTime DateOflusse { get; set; }
         public GradeScale GradeValue { get; set; }
         public Subject Subject { get; set; }
--- SchoolRegister/SchoolRegister.BLL/Entities/Student.cs
+++ SchoolRegister/SchoolRegister.BLL/Entities/Student.cs
@@ -1,14 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 using System.Text;
 
 namespace SchoolRegister.BLL.Entities
 {
    public class Student : User
     {
-        public double AverageGrade { get; }
-        public IDictionary<string, double> AverageGradePerSubject { get; }
+        [NotMapped]
+        public double AverageGrade => Math.Round(Grades
+            .Average(g => (int)g.GradeValue), 1);
+        [NotMapped]
+        public IDictionary<string, double> AverageGradePerSubject => Grades
+             .GroupBy(g => g.Subject.Name)
+             .Select(g => new {
+                 SubjectName = g.Key,
+                     AvgGrande = Math.Round(
+                          g.Average(AverageGrade => (int)AverageGrade.GradeValue), 1
+                          )
+                })
+            .ToDictionary(AverageGrade => AverageGrade.SubjectName, AverageGrade => AverageGrade.AvgGrande);
         public IList<Grade> Grades { get; set; }
         public Group Group { get; set; }
+        [ForeignKey("Group")]
+        public int GruopId { get; set; }
+
+        public IList<Student> Students { get; set; }
+
+
+      
     }
 }
