3f33e49135652f59f000a43270fcaa88072c176b
Przemyslaw <przemy@o2.pl>
2020-03-10 12:00:34 +0100

    lab2

---
+++ SchoolRegister/SchoolRegister.BLL/Entities/SubjectGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.BLL.Entities
+{
+    class SubjectGroup
+    {
+        public Group Group { get; set; }
+        public int GroupId { get; set; }
+        public Subject Subject { get; set; }
+        public int SubjectId { get; set; }
+    }
+}
