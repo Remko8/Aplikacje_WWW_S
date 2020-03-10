fafbe155c8cf5c6fd455cffb70b1ab967e71d4bb
Mariusz J <mariuszjag@o2.pl>
2020-03-10 12:04:54 +0100

    2020-03-10

--- SchoolRegister/SchoolRegister.BLL/Entities/Group.cs
+++ SchoolRegister/SchoolRegister.BLL/Entities/Group.cs
@@ -9,5 +9,7 @@ namespace SchoolRegister.BLL.Entities
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public IList<Student> Students { get; set; }
+
     }
 }
---
+++ SchoolRegister/SchoolRegister.DAL/EF/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SchoolRegister.BLL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.DAL.EF
+{
+    public class ApplicationDbContext : IdentityDbContext<User, Role, int>
+    {
+        private readonly ConnectionStringDto _connectionStringDto; // connection string
+        // Table properties e.g
+        public virtual DbSet<Grade> Grade { get; set; }
+        // other table properties
+        // ……
+        //public DbSet<Post> Posts { get; set; } // example table property
+        //public DbSet<Comment> Comments { get; set; }
+        public ApplicationDbContext(ConnectionStringDto connectionStringDto) // constructor
+        {
+            _connectionStringDto = connectionStringDto;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(_connectionStringDto.ConnectionString); // for provider SQL Server 
+    }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Fluent API commands
+            modelBuilder.Entity<User>() // defining a one to many relation in fluent API
+                        .ToTable("AspNetUsers")
+                        .HasDiscriminator<int>("UserType")
+                        .HasValue<Student>(1)
+                        .HasValue<Parent>(2)
+                        .HasValue<Teacher>(3);
+        }
+    }
+}
