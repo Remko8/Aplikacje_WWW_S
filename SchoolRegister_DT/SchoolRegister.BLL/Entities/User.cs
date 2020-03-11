db8f9f6ce89659f07046aa68ed5f18303f4108c6
DTroczyk <DTroczyk@users.noreply.github.com>
2020-03-11 10:59:53 +0100

    First commit

---
+++ SchoolRegister_DT/SchoolRegister.BLL/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.BLL.Entities
+{
+    class User
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        DateTime RegistrationDate { get; set; }
+    }
+}
