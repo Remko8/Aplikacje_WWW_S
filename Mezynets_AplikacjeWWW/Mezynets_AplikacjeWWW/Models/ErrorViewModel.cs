e2262f3da9347a751fc4f4b85720534f80a60ae0
Bohdan M <mznbogdan@gmai.com>
2020-03-11 15:30:12 +0100

    commit

---
+++ Mezynets_AplikacjeWWW/Mezynets_AplikacjeWWW/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mezynets_AplikacjeWWW.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
