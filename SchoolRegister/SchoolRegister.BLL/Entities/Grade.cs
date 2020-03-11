f793b44a6136f8ac51d5a0fe0a8c945b582f7d82
Xando <milosz.latani222k@wp.pl>
2020-03-11 17:59:46 +0100

    LAB2

---
+++ SchoolRegister/SchoolRegister.BLL/Entities/Grade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SchoolRegister.BLL.Entities
+{
+    public class Grade
+    {
+        [Key]
+        public DateTime DateOfIssue { get; set; }
+        public GradeScale GradeValue { get; set; }
+        public Subject Subject { get; set; }
+    }
+}
---
+++ SchoolRegister/SchoolRegister.BLL/Entities/Parent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.BLL.Entities
+{
+    public class Parent : User
+    {
+        public List<Student> Students { get; set; }
+    }
+}
