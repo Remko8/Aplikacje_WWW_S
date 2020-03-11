8c78993dc4802f9dd0558c32f57087145c9fe7ea
Natalia Olas <n.olasik@gmail.com>
2020-03-11 17:59:44 +0100

    Lab03

---
+++ SchoolRegister/SchoolRegister.BLL/Entities/Group.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.BLL.Entities
+{
+   public  class Group
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+
+        public IList<Student> Students { get; set; }
+
+    }
+}
---
+++ SchoolRegister/SchoolRegister.BLL/Entities/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using System.Linq;
+
+namespace SchoolRegister.BLL.Entities
+{
+    public class Student:User
+    {
+      
+        public IList<Grade> Grades { get; set; }
+
+        public Group Group { get; set; }
+        [ForeignKey("Group")]
+        public int GroupId { get; set; }
+
+        [NotMapped]
+        public double AverageGrade => Math.Round(Grades.Average(g => (int)g.GradeValue),1);
+
+        public IDictionary<string, double> AverageGradePerSubject => Grades
+          .GroupBy(g => g.Subject.Name)
+          .Select(g => new
+          {
+              SubjectName = g.Key,
+              AvgGrade = Math.Round(
+              g.Average(avg => (int)avg.GradeValue)
+              , 1)
+          })
+          .ToDictionary(avg => avg.SubjectName, avg => avg.AvgGrade);
+
+
+    }
+}
