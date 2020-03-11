60f1a757cfc7f63939c92493c1bdd41600009eed
Adrian Chmielewski <mradex77@gmail.com>
2020-03-11 09:46:27 +0100

    v1.0

---
+++ SchoolRegister/SchoolRegister/SchoolRegister.BLL/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.BLL.Entities
+{
+    abstract public class User
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        DateTime RegistrationDate { get; set; }
+    }
+}
