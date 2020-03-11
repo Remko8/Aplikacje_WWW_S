14797d2e24f45ea5da7cebb14c99d264b467dbaf
Piotr R <FakeEmail@fake.com>
2020-03-11 17:59:48 +0100

    Lab2

--- SchoolRegister/SchoolRegister.BLL/Entities/User.cs
+++ SchoolRegister/SchoolRegister.BLL/Entities/User.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace SchoolRegister.BLL.Entities
 {
-    class User:IdentityUser<int>
+    public class User:IdentityUser<int>
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
