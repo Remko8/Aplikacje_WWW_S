0a043ab593a7760c08e40cc3b8ffc52e6922b8b8
MateuszD <mateusz.dobosz3@gmail.com>
2020-03-11 09:41:00 +0100

    Lab 1

---
+++ SchoolRegister/SchoolRegister/SchoolRegister.BLL/Entities/Student.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.BLL.Entities
+{
+    class Student : User
+    {
+        double AvarageGrade { get; }
+        IDictionary<string,double> AvarageGradePerSubject { get; }
+        IList<Grade> Grades { get; set; }
+        Group Group { get; set; }
+    }
+}
