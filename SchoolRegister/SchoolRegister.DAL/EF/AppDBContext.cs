72ca52e7d2a214c6608b652a1b5e53eb13fe306f
Lukasz Kowalik <lukasz.kowalik41@gmail.com>
2020-03-11 15:27:02 +0100

    lab2 start

---
+++ SchoolRegister/SchoolRegister.DAL/EF/AppDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolRegister.DAL.EF
+{
+    class AppDBContext
+    {
+        private readonly ConnectionStringDto _connectionStringDto;
+
+        public AppDBContext(ConnectionStringDto connectionString)
+        {
+            _connectionStringDto = connectionString;
+        }
+        protected override void OnConfiguring()
+        {
+
+        }
+    }
+}
