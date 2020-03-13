f64e6a3906963bcf47845a7a84e68e9d11f9011e
Dawid P <dawidpiatek97@gmail.com>
2020-03-13 16:58:06 +0100

    Ver 1.0

--- SchoolRegister/SchoolRegister.BLL/Entities/Parent.cs
+++ SchoolRegister/SchoolRegister.BLL/Entities/Parent.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace SchoolRegister.BLL.Entities
 {
     public class Parent : User
     {
         public IList<Student> Students { get; set; }
+
     }
 }
--- SchoolRegister/SchoolRegister.BLL/Entities/Student.cs
+++ SchoolRegister/SchoolRegister.BLL/Entities/Student.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Text;
 
 namespace SchoolRegister.BLL.Entities
 {
     public class Student : User
     {
+        public IList<Grade> Grades { get; set; }
         public Group Group { get; set; }
 
-        public IList<Grade> Grades { get; set; }
-
         [NotMapped]
-        public double AverageGrade => Math.Round(Grades.Average(g => (int)g.GradeValue),1);
+        public double AverageGrade => Math.Round(Grades.Average(g => (int)g.GradeValue), 1);
 
         [NotMapped]
         public IDictionary<string, double> AverageGradePerSubject => Grades
             .GroupBy(g => g.Subject.Name)
-            .Select(g => new {SubjectName = g.Key, AvgGrade = Math.Round(g.Average(avg => (int) avg.GradeValue), 1)})
+            .Select(g => new { SubjectName = g.Key, AvgGrade = Math.Round(g.Average(avg => (int)avg.GradeValue), 1) })
             .ToDictionary(avg => avg.SubjectName, avg => avg.AvgGrade);
     }
 }
